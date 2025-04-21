c38a4ec11c487f5d1add91cd37c13ab7f9cf100a
stevanussatria <stevanus.satria@workato.com>
2025-04-21 18:43:08 +0800

    Feat: add verifiable links to milestones

--- docs/components/Timeline.vue
+++ docs/components/Timeline.vue
@@ -18,6 +18,13 @@
       <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         {{ item.description }}
       </p>
+      <div v-show="item.path"><a target="_blank" :href="item.path"
+         class="inline-flex no-underline! hover:underline items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">{{ item.cta }}
+         <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+           viewBox="0 0 14 10">
+           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             d="M1 5h12m0 0L9 1m4 4L9 9" />
+         </svg></a></div>
     </li>
   </ol>
 </template>
@@ -27,6 +34,8 @@ interface TimelineItem {
   time: string;
   title: string;
   description: string;
+  path?: string;
+  cta?: string;
 }
 
 const props = withDefaults(
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,6 @@ export default {
     "./docs/.vuepress/**/*.{vue,js,ts,md}",
     "./docs/**/*.{vue,js,ts,md}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "#0d0d0d",
-        accent: "#8affc1",
-        muted: "#999999",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
