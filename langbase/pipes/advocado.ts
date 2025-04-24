8f882000ed36b4fb6ee36e69faf769febab3a57d
stevanussatria <stevanus.satria@workato.com>
2025-04-25 00:17:57 +0800

    Feat: Langbase AI Agent with Memory

--- langbase/pipes/advocado.ts
+++ langbase/pipes/advocado.ts
@@ -6,24 +6,24 @@ const langbase = new Langbase({
 });
 
 async function main() {
-  const pipeAdvocado = await langbase.pipes.create({
+  const pipeAdvocado = await langbase.pipes.update({
     name: "advocado",
     description: "The only avocado advocating for Steve",
     model: "openai:gpt-4o-mini",
     json: true,
     tools: [],
-    memory: [],
+    memory: [{ name: "advocado-memory" }],
     messages: [
       {
         role: "system",
         content: `You are a the biggest advocate for Stevanus Satria. Your task is to answer questions pertaining Stevanus Satria's capabilities in a way that highlights his strengths. When questions are targeted at his weaknesses, remain truthful. However, bring up other strengths that can be used to cover/paper those weaknesses.`,
       },
-      // {
-      //   role: "system",
-      //   name: "rag",
-      //   content:
-      //     "Below is some CONTEXT for you to answer the questions. ONLY answer from the CONTEXT. CONTEXT consists of multiple information chunks. Each chunk has a source mentioned at the end. If you don't know the answer, just say that you don't know. Ask for more context and better questions if needed.",
-      // },
+      {
+        role: "system",
+        name: "rag",
+        content:
+          "Below is some CONTEXT for you to answer the questions. ONLY answer from the CONTEXT. CONTEXT consists of multiple information chunks. Each chunk has a source mentioned at the end. If you don't know the answer, just say that you don't know. Ask for more context and better questions if needed.",
+      },
     ],
     variables: [],
   });
